e74120d4490a723b7f87d0662b704813cc37021b
wsd-baobao <a2245828322@hotmail.com>
2024-04-06 11:06:11 +0800

    奥术工作台基本上可以使用了，炼金桌不能用，

--- src/main/java/dev/latvian/mods/projectex/integration/jei/ArcaneTabletTransfer.java
+++ src/main/java/dev/latvian/mods/projectex/integration/jei/ArcaneTabletTransfer.java
@@ -7,8 +7,6 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.gui.ingredient.IGuiIngredient;
-import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
-import mezz.jei.api.recipe.IFocus;
 import mezz.jei.api.recipe.transfer.IRecipeTransferError;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
@@ -19,10 +17,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ArcaneTabletTransfer implements IRecipeTransferHandler<ArcaneTabletMenu> {
@@ -46,18 +41,36 @@ public class ArcaneTabletTransfer implements IRecipeTransferHandler<ArcaneTablet
     @Nullable
     @Override
     public IRecipeTransferError transferRecipe(ArcaneTabletMenu container, Object recipe, IRecipeLayout recipeLayout, Player player, boolean maxTransfer, boolean doTransfer) {
-
-        System.out.println(container);
-        System.out.println(recipe);
-        System.out.println(recipeLayout);
-        System.out.println(player);
-        System.out.println(maxTransfer);
         if (doTransfer) {
+            if (recipe instanceof CraftingRecipe){
 
 
 
+            Map<Integer, ? extends IGuiIngredient<ItemStack>> guiIngredients = recipeLayout.getItemStacks().getGuiIngredients();
+            Map<Integer, ItemStack> temp = new HashMap<>();
+            for (int i = 0; i < guiIngredients.size()-1; i++){
+                if (guiIngredients.get(i+1).getDisplayedIngredient()==null){
+                    temp.put(i, ItemStack.EMPTY);
+                }else {
+                    temp.put(i, guiIngredients.get(i+1).getDisplayedIngredient());
+                }
+            }
+            List<Slot> slots = container.slots
+                    .stream().filter(s -> s.container instanceof CraftingContainer)
+                    .collect(Collectors.toList());
+            System.out.println(slots);
+            System.out.println(temp);
+            if (slots.size() != temp.size()){
+                return transferHelper.createInternalError();
+            }
+            Int2ObjectMap<List<ItemStack>> stacksMap = new Int2ObjectOpenHashMap<>();
+            for (int i = 0; i < temp.size(); i++){
+                stacksMap.put(i, Collections.singletonList((temp.get(i))));
+            }
 
-            NetworkHandler.sendToServer(new PacketArcaneTabletRecipeTransfer(null, maxTransfer));
+
+            NetworkHandler.sendToServer(new PacketArcaneTabletRecipeTransfer(stacksMap, maxTransfer));
+        }
         }
 
         return null;
